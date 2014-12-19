4a3e09fcecf5463c5e3b963d1cdab9c281806fb1
Roger Haris <viomab@hotmail.com>
2014-12-19 15:54:20 +0800

    测试metro flyout 的一个例子

---
+++ testlayout/FlyoutDemoView.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace testlayout
+{
+    /// <summary>
+    /// FlyoutDemo.xaml 的交互逻辑
+    /// </summary>
+    public partial class FlyoutDemoView : MetroWindow
+    {
+        public FlyoutDemoView()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowSettings(object sender, RoutedEventArgs e)
+        {
+            this.ToggleFlyout(6);
+        }
+        private void ShowSettingsLeft(object sender, RoutedEventArgs e)
+        {
+            var flyout = (Flyout)this.Flyouts.Items[6];
+            flyout.Position = Position.Left;
+        }
+        private void ShowLeft(object sender, RoutedEventArgs e)
+        {
+            this.ToggleFlyout(6);
+        }
+
+        private void ToggleFlyout(int index)
+        {
+            var flyout = this.Flyouts.Items[index] as Flyout;
+            if (flyout == null)
+            {
+                return;
+            }
+
+            flyout.IsOpen = !flyout.IsOpen;
+        }
+    }
+}
---
+++ testlayout/FlyoutDemoViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace testlayout
+{
+    public class FlyoutDemoViewModel:PropertyChangedBase
+     
+    {
+       //public FlyoutDemoViewModel()
+       // { }
+       // private void ShowSettings(object sender, RoutedEventArgs e)
+       // {
+       //     this.ToggleFlyout(6);
+       // }
+       // private void ShowSettingsLeft(object sender, RoutedEventArgs e)
+       // {
+       //     var flyout = (Flyout)this.Flyouts.Items[6];
+       //     flyout.Position = Position.Left;
+       // }
+       // private void ShowLeft(object sender, RoutedEventArgs e)
+       // {
+       //     this.ToggleFlyout(6);
+       // }
+
+       // private void ToggleFlyout(int index)
+       // {
+       //     var flyout = this.Flyouts.Items[index] as Flyout;
+       //     if (flyout == null)
+       //     {
+       //         return;
+       //     }
+
+       //     flyout.IsOpen = !flyout.IsOpen;
+       // }
+    }
+}
